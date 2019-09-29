87b3d32d360185e8aed75da78f76f9937fb00655
martindelosrios <martindelosrios13@gmail.com>
2019-09-28 22:40:46 -0300

    minor changes

--- R/FeaturesEstimationFunctions.R
+++ R/FeaturesEstimationFunctions.R
@@ -381,7 +381,7 @@ get_cluster_features <- function(dat, ntotal, name.groups){
   counter  <- 0
   ngal.lim <- 30
 
-  pb <- progress_bar$new(total = ntotal)
+  pb <- progress_bar$new(total = floor(ntotal/30))
   for(i in 1:ntotal){
     pb$tick()
 
@@ -597,8 +597,8 @@ messi <- function(cat,
                   galaxiesOutput = 'galaxiesOutput.dat',
                   classOutput    = 'ClassOutput.dat',
                   folder    = 'folder', 
-                  ClustersModel = 'defaultClustersModel',
-                  GalaxiesModel = 'defaultGalaxiesModel',
+                  ClustersML = 'defaultClustersModel',
+                  GalaxiesML = 'defaultGalaxiesModel',
                   #TrainsetClusters = 'trainset_cum.dat',
                   #TrainsetGalaxies = 'trainset_gal.dat',
                   ntotal = 0, 
@@ -632,14 +632,20 @@ messi <- function(cat,
   }
   if(classification == TRUE){
     print('Starting the classification of the galaxy clusters')
-    if(ClustersModel == 'defaultClustersModel'){MLModel <- data(ClustersModel)}
-    Classification       <- get_clusters_classification(ClustersData, MLModel)
+    if(ClustersML == 'defaultClustersModel'){ 
+      print('Using Defatult Model')    
+      data(ClustersModel)
+    }
+    Classification       <- get_clusters_classification(ClustersData, ClustersModel)
     ClustersData$relProb <- Classification[,1]
     ClustersData$merProb <- Classification[,2]
 
     print('Starting the estimation of the substructures')
-    if(GalaxiessModel == 'defaultGalaxiessModel'){MLModel <- data(GalaxiesModel)}
-    Substructures <- get_substructures(ClustersData = ClustersData, GalaxiesData = GalaxiesData, model = MLModel, probLimit = probLimit, folder = folder)
+    if(GalaxiesML == 'defaultGalaxiessModel'){
+      print('Using default Model')
+      data(GalaxiesModel)
+    }
+    Substructures <- get_substructures(ClustersData = ClustersData, GalaxiesData = GalaxiesData, model = GalaxiesModel, probLimit = probLimit, folder = folder)
     if(Substructures$group.id[1] != -99){
       names <- c('group.id' ,'FirstSubs', 'SecondSubs', 'rvir1', 'rvir2', 'dvel1', 'dvel2', 'mas1', 'mas2',
          'par1', 'par2', 'tot', 'racen1', 'racen2', 'deccen1', 'deccen2', 'velcen1', 'velcen2')
